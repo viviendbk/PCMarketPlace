6fa9c59758b7458fd42b5956c6c1130295ad81af
SantiagoMartin2002 <santiago.martin@edu.devinci.fr>
2023-12-16 20:21:41 +0100

    import Angular Materials and started sign up page

--- front-end/src/app/login-page/login-page.component.ts
+++ front-end/src/app/login-page/login-page.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
 
+export class LoginPageComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value)
+      this.http.post('/api/login', this.loginForm.value)
+        .subscribe(
+          response => {
+            console.log('Login successful', response);
+            // Handle successful login here
+          },
+          error => {
+            console.error('Wrong credentials', error);
+            // Handle login failure here
+          }
+        );
+    }
+  }
 }
